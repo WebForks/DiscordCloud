918e133b6db15a174be50c33e8356b4620f3a78e
WebForks <35718739+WebForks@users.noreply.github.com>
2024-02-10 21:59:46 -0800

    added path

--- db-creator.py
+++ db-creator.py
@@ -1,8 +1,8 @@
 import sqlite3
 import os
 
-db_path = r""
-'''
+db_path = r"./db/file.db"
+
 # CREATES NEW DB
 # Define the connection and cursor
 conn = sqlite3.connect(db_path)
@@ -10,14 +10,14 @@ cursor = conn.cursor()
 
 # SQL statement for creating a new SQLite database table
 create_table_query = '''
-# CREATE TABLE IF NOT EXISTS files (
-#    FileName TEXT,
-#    TimeUploaded TEXT,
-#    FileSize INTEGER,
-#    DiscordLink TEXT, -- Storing as TEXT because SQLite does not support array type natively
-#    FileSplitAmount INTEGER,
-#    FileSplitNames TEXT -- Storing as TEXT because SQLite does not support array type natively
-# )
+ CREATE TABLE IF NOT EXISTS files (
+    FileName TEXT,
+    TimeUploaded TEXT,
+    FileSize INTEGER,
+    DiscordLink TEXT, -- Storing as TEXT because SQLite does not support array type natively
+    FileSplitAmount INTEGER,
+    FileSplitNames TEXT -- Storing as TEXT because SQLite does not support array type natively
+ )
 '''
 
 # Execute the query
@@ -28,6 +28,7 @@ conn.commit()
 conn.close()
 
 print("Database and table created successfully.")
+
 '''
 # DELETS ALL ROWS IN DB
 
@@ -49,3 +50,4 @@ cursor.execute(create_table_sql)
 # Commit the changes and close the connection
 conn.commit()
 conn.close()
+'''
